1077af0595d4750d836f14111cdb2047efa0cf97
Paul <badeapaul3@gmail.com>
2025-03-25 10:59:08 +0200

    First Commit

---
+++ src/main/java/com/inventory/main/Main.java
@@ -0,0 +1,12 @@
+package com.inventory.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Inventory Management System started...");
+    }
+}
